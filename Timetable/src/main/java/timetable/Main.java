91fe93abed03588eee36ba94250a8c56e8a5f79e
Tobias <Tobias@EnvyX360>
2019-01-25 11:43:55 +0100

    fixed highlighting bug in timetable menu

--- Timetable/src/main/java/timetable/Main.java
+++ Timetable/src/main/java/timetable/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("Timatable");
+        stage.setTitle("TTable");
         stage.getIcons().add(new Image("/images/Timetable.png"));
         stage.setScene(scene);
         stage.show();
