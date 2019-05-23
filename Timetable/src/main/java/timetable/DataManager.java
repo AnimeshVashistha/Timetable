393ff17166a556bd650bbbdc97af782b2210c6af
user <Tobias@Computer>
2019-05-23 23:11:55 +0200

    working on datamanager

--- Timetable/src/main/java/timetable/DataManager.java
+++ Timetable/src/main/java/timetable/DataManager.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import timetable.Datatypes.SimpleTime;
+import timetable.Datatypes.Subject;
 import timetable.Datatypes.Timetable;
 import timetable.Datatypes.TimetablePair;
 
@@ -74,30 +76,77 @@ public class DataManager {
         }
     }
 
+    final static String TIMETABLE_PAIR_NAME = "name";
+    final static String TIMETABLE_PAIR_A = "a";
+    final static String TIMETABLE_PAIR_B = "b";
+
     public JSONObject convertTimetablePair(TimetablePair timetablePair) {
         JSONObject tp = new JSONObject();
-        tp.put("name", timetablePair.getName());
-        tp.put("a", convertTimetable(timetablePair.getA()));
-        tp.put("b", convertTimetable(timetablePair.getB()));
-        
+        tp.put(TIMETABLE_PAIR_NAME, timetablePair.getName());
+        tp.put(TIMETABLE_PAIR_A, convertTimetable(timetablePair.getA()));
+        tp.put(TIMETABLE_PAIR_B, convertTimetable(timetablePair.getB()));
+
         return tp;
     }
-    
-    public JSONObject convertTimetable(Timetable timetable){
+
+    final static String TIMETABLE_DAYS = "days";
+    final static String TIMETABLE_TIMES = "times";
+    final static String TIMETABLE_SUBJECTS = "SUBJECTS";
+
+    public JSONObject convertTimetable(Timetable timetable) {
         JSONObject t = new JSONObject();
-        
+
         JSONArray days = new JSONArray();
+        for (boolean b : timetable.getDays()) {
+            days.add(b);
+        }
+        t.put(TIMETABLE_DAYS, days);
         JSONArray times = new JSONArray();
+        for (SimpleTime st : timetable.getTimes()) {
+            times.add(convertSimpleTime(st));
+        }
+        t.put(TIMETABLE_TIMES, times);
         JSONArray subjects = new JSONArray();
-        JSONObject startTime = new JSONObject();
-        
+        for (Subject[] sa : timetable.getSubjects()) {
+            JSONArray subjectsInner = new JSONArray();
+            for (Subject s : sa) {
+                subjectsInner.add(convertSubject(s));
+            }
+            subjects.add(sa);
+        }
+        t.put(TIMETABLE_SUBJECTS, convertSimpleTime(timetable.getStartTime()));
+
         t.put("lessons", timetable.getLessons());
         t.put("smallPause", timetable.getSmallPause());
         t.put("middlePause", timetable.getMiddlePause());
         t.put("bigPause", timetable.getBigPause());
         t.put("lessonLenght", timetable.getLessonlength());
-        
+
         return t;
+    }
+
+    final static String SIMPLE_TIME_HOURS = "hours";
+    final static String SIMPLE_TIME_MINUTES = "minutes";
+
+    public JSONObject convertSimpleTime(SimpleTime simpleTime) {
+        JSONObject st = new JSONObject();
+        st.put(SIMPLE_TIME_HOURS, simpleTime.getHours());
+        st.put(SIMPLE_TIME_MINUTES, simpleTime.getMinutes());
+
+        return st;
+    }
+
+    final static String SUBJECT_NAME = "name";
+    final static String SUBJECT_ROOM = "room";
+    final static String SUBJECT_TEACHER = "teacher";
+
+    public JSONObject convertSubject(Subject subject) {
+        JSONObject s = new JSONObject();
+        s.put(SUBJECT_NAME, subject.getSubject());
+        s.put(SUBJECT_ROOM, subject.getRoom());
+        s.put(SUBJECT_TEACHER, subject.getTeacher());
+
+        return s;
     }
 
     public void readData() {
